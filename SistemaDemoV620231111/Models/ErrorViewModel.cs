b7935669dd9af8c1a480290e9313032e615f8c4a
ROLANDO GONZALES CESPEDES <net.gonzales@gmail.com>
2023-11-11 02:36:57 -0400

    first commit

---
+++ SistemaDemoV620231111/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaDemoV620231111.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
